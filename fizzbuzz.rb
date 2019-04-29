6a5ae11a152c179a5a7101d0e1d9c9802104f41e
Jooyon Won <jyon1204@gmail.com>
2019-04-29 05:02:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,15 @@
 def fizzbuzz(int)
   if(int % 3 == 0)
     return "Fizz";
+    
   elsif(int % 5 == 0)
     return "Buzz";
+    
   elsif(int % 3 && 5 == 0)
     return "FizzBuzz";
+    
   elsif !(int% 3 || 5)
     return "nil";
+    
 end
 end
