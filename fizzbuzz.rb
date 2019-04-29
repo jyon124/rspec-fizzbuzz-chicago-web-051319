83be4b9b4cba9480af6698a1964a8ed4a5e37f92
Jooyon Won <jyon1204@gmail.com>
2019-04-29 04:34:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(phrase)
+  if (phrase == 3)
+    return "Fizz"
+  elsif (phrase == 5)
+    return "Buzz"
+  elsif (phrase == 3 && phrase == 5)
+    return "FizzBuzz"
+  else
+    return "nil"
+end
